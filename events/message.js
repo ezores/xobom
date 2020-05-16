9c6b4fe6395f16240919360cffa1043ab9b97b98
Farid <61091039+z0dic@users.noreply.github.com>
2020-05-16 16:41:07 +0400

    fredx

--- events/message.js
+++ events/message.js
@@ -1,9 +1,9 @@
-const ayarlar = require('../ayarlar.json');
+const settings = require('../settings.json');
 module.exports = message => {
   let client = message.client;
   if (message.author.bot) return;
-  if (!message.content.startsWith(ayarlar.prefix)) return;
-  let command = message.content.split(' ')[0].slice(ayarlar.prefix.length);
+  if (!message.content.startsWith(settings.prefix)) return;
+  let command = message.content.split(' ')[0].slice(settings.prefix.length);
   let params = message.content.split(' ').slice(1);
   let perms = client.elevation(message);
   let cmd;
